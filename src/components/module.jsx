6549f9b4fa78a51e6973af9a166cc3ce937901e5
Simon Reinhardt <saadabou.study@gmail.com>
2022-08-31 19:17:05 +0100

    first commit

---
+++ src/components/module.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Module = () => {
+  return (
+    <div className='module'>
+      {/*  shape  */}
+      <div className='module__shape'>
+        <div className="module__shape-shadow-1"></div>
+        <div className="module__shape-shadow-2"></div>
+
+        <div className='module__shape-rec1'></div>
+        <div className='module__shape-rec2'></div>
+        <div className='module__shape-rec3'></div>
+        <div className='module__shape-rec4'></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Module
